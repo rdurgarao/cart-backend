c6af8e357034501e1bc56d1d0973edd042f57376
Durga R <raod1989r@gmail.com>
2021-04-23 09:05:25 +0530

    Added login functionality

--- server.rb
+++ server.rb
@@ -37,3 +37,17 @@ end
 get '/cart' do 
     @@cart.to_json
 end
+
+post '/login' do 
+    @user_details = JSON.parse(request.body.read, symbolize_names: true)
+    @user_files = File.read "./users.json"
+    @user_files_json = JSON.parse @user_files
+
+    @user = @user_files_json.select {|user| user['emailOrPhone'].eql?@user_details[:email]}.first
+
+    if @user && @user['password'].eql?(@user_details[:password])
+        {status: 'success', user: @user}.to_json
+    else 
+        {status: 'failure', message: 'Invalid credentials'}.to_json
+    end
+end
